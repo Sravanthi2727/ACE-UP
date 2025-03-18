cc114a36061848bd0439262830351ad89df39f5d
SRAVANTHI VADLA <137860943+Sravanthi2727@users.noreply.github.com>
2025-03-18 23:12:42 +0530

    Delete Filter page directory
    Cuz its already written in main HTML file

--- Filter page/app.js
+++
@@ -1,6 +0,0 @@
-let value= document.getElementById("live-value");
-let price=document.getElementById("price");
-value.innerHTML=price.value;
-price.addEventListener("input",function(){
-    value.innerHTML=price.value;
-});
