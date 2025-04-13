86c5c6e2a68af8ebff3b9909a3f8e4a4c3fd33de
SravanthiVadla27 <sravanthi2706@gmail.com>
2025-04-13 14:26:18 +0530

    Search button added

--- app.js
+++ app.js
@@ -5,17 +5,15 @@ const cookieParser = require("cookie-parser");
 const expressLayout = require("express-ejs-layouts");
 const port = 3000;
 
-app.set("view engine","ejs");
-app.set("layout","layouts/main");
-app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine", "ejs");
+app.set("layout", "layouts/main");
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(expressLayout)
-app.use(cookieParser())
-app.use("/",require("./server/route/main"));
+app.use(express.urlencoded({ extended: true }));
+app.use(expressLayout);
+app.use(cookieParser());
+app.use("/", require("./server/route/main"));
 
-
-app.listen(port,()=>{
-    console.log(`app is running on ${port} port`);
-
-})
+app.listen(port, () => {
+  console.log(`app is running on ${port} port`);
+});
